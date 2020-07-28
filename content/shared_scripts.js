ba892c5d13e08bb61a82a8ae41148d675f90366b
Cambysses <selxxa@gmail.com>
2020-07-28 15:26:48 -0300

    Update index
    navigation is now handled by making content visible/not-visible instead of hrefs, now music will persist throughout site

--- content/shared_scripts.js
+++ content/shared_scripts.js
@@ -1,19 +1,27 @@
 $(window).on('load', function() 
 {
 	$("body").fadeIn(1000);
-	
-	$("a.transition").click(function(event)
-	{
-		event.preventDefault();
-		linkLocation = this.href;
-		$("body").fadeOut(500, redirectPage);      
-	});
+	$(".index-container").fadeIn(1000);
+	// $("a.transition").click(function(event)
+	// {
+		// event.preventDefault();
+		// linkLocation = this.href;
+		// $("body").fadeOut(500, redirectPage);      
+	// });
 
-	function redirectPage() 
-	{
-		window.location = linkLocation;
-	}
+	// function redirectPage() 
+	// {
+		// window.location = linkLocation;
+	// }
 });
+
+function loadContent(content)
+{
+	$(".content").fadeOut(500).promise().done(function()
+	{
+		$(`.${content}-container`).fadeIn(1000);
+	});	
+}
 
 function toggleAudio()
 {
