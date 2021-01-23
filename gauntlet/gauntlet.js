8e1c38de427c9acf769b295731cc5e434cb4f2f2
Cambysses <selxxa@gmail.com>
2021-01-22 21:33:13 -0400

    fix array typo

--- gauntlet/gauntlet.js
+++ gauntlet/gauntlet.js
@@ -1,7 +1,7 @@
 async function getRandomGame (platform) {
     let games = await fetch(`games/${platform}.txt`).then(response => response.text());
     
-    let supportedPlatforms = ("nes","snes","n64","gb","gbc","gba","mastersystem","genesis","dreamcast","gamegear","tg16","ps1");
+    let supportedPlatforms = ["nes","snes","n64","gb","gbc","gba","mastersystem","genesis","dreamcast","gamegear","tg16","ps1"];
     if ($("#includeJapanese").prop("checked") && supportedPlatforms.includes(platform))
     {
         games += await fetch(`games/${platform}_jpn.txt`).then(response => response.text());
