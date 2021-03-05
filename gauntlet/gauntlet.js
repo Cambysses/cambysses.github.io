c29761a4663f6d33de3ecd855157f3699a77c94e
Cambysses <selxxa@gmail.com>
2021-03-05 13:42:59 -0400

    add gw360 site

--- gauntlet/gauntlet.js
+++ gauntlet/gauntlet.js
@@ -1,10 +1,10 @@
-async function getRandomGame (platform) {
-    let games = await fetch(`games/${platform}.txt`).then(response => response.text());
-    
-    let supportedPlatforms = ["nes","snes","n64","gb","gbc","gba","mastersystem","genesis","dreamcast","gamegear","tg16","ps1"];
-    if ($("#includeJapanese").prop("checked") && supportedPlatforms.includes(platform))
-    {
-        games += await fetch(`games/${platform}_jpn.txt`).then(response => response.text());
+async function getRandomGame(platform) {
+    let games = await fetch(`content/games/${platform}.txt`).then(response => response.text());
+
+    // Adds Japanese games to list, if checked and available.
+    let supportedPlatforms = ["nes", "snes", "n64", "gb", "gbc", "gba", "mastersystem", "genesis", "dreamcast", "gamegear", "tg16", "ps1"];
+    if ($("#includeJapanese").prop("checked") && supportedPlatforms.includes(platform)) {
+        games += await fetch(`content/games/${platform}_jpn.txt`).then(response => response.text());
     }
 
     games = games.split('\n');
@@ -14,13 +14,11 @@ async function getRandomGame (platform) {
 $(function () {
     $("#game-announcer").hide();
     $("#animated").attr("src", `content/sprites/${Math.floor(Math.random() * 25)}.gif`);
-    $("#game-select ul li").click(function (e) 
-    {
-        $('#pre-game').html(`Your ${$(this).text()} game is`);
-        (async () =>
-        {
+    $("#game-select ul li").click(function (e) {
+        (async () => {
             $('#game').html(await getRandomGame($(this).attr('id')));
         })()
+        $('#pre-game').html(`Your ${$(this).text()} game is`);
         $('#game-announcer').show();
     });
 });
